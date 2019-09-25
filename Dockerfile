ff9c29a1c633c8c767f8d11a7913b1ef0245e3fc
Tom Wu <tom@devopsify.io>
2019-09-24 23:46:22 -0400

    add apm

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ EXPOSE 8099
 
 COPY build/libs/voter-service-0.2.0.jar /opt/
 
+COPY libs/elastic-apm-agent-1.9.0.jar /opt/
+
 COPY docker-init.sh /opt/
 
 CMD /opt/docker-init.sh 
--- docker-init.sh
+++ docker-init.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-java -jar /opt/voter-service-0.2.0.jar \
-	--spring.profiles.active=${SPRING_PROFILE:-docker-production} \
-        --spring.data.mongodb.host=${MONGODB_HOST:-mongodb} 
-
+if [ "${ENABLE_ELASTIC_APM_AGENT}" = true ]; then
+        java -javaagent:/opt/elastic-apm-agent-1.9.0.jar \
+                -Delastic.apm.service_name=apm-agent-test-voter-service \
+                -Delastic.apm.application_packages=org.example,com.example \
+                -Delastic.apm.server_urls=${ELASTIC_APM_SERVER_URLS} \
+                -jar /opt/voter-service-0.2.0.jar \
+                --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
+                --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
+else
+        java -jar /opt/voter-service-0.2.0.jar \
+                --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
+                --spring.data.mongodb.host=${MONGODB_HOST:-mongodb} 
+fi
