89cc676ae51eb7b25b6da344d056b7c596c31e9a
Tom Wu <tom@devopsify.io>
2019-10-01 16:09:42 -0400

    add apm for api integration

--- src/main/java/com/example/voter/Application.java
+++ src/main/java/com/example/voter/Application.java
@@ -3,10 +3,15 @@ package com.example.voter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import co.elastic.apm.attach.ElasticApmAttacher;
+import org.springframework.boot.SpringApplication;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+        ElasticApmAttacher.attach();
+        
         SpringApplication.run(Application.class, args);
     }
 }
