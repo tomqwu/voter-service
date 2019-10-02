b4a5462871d58365e9012c018ccbc016fd18e2ba
Tom Wu <tom@devopsify.io>
2019-10-02 13:18:56 -0400

    add flag for javaagent

--- docker-init.sh
+++ docker-init.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-java -jar /opt/voter-service-0.2.0.jar \
-        --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
-        --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
+if [ ${ENABLE_ELASTIC_JAVAAGENT} = "true" ];
+then
+        java -javaagent:/opt/elastic-apm-agent-1.9.0.jar
+                -jar /opt/voter-service-0.2.0.jar \
+                --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
+                --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
+
+else
+        java -jar /opt/voter-service-0.2.0.jar \
+                --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
+                --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
+
+fi
