e2d374b3f776b30641dd757e6f13af3c97d85443
Tom Wu <tom@devopsify.io>
2019-10-02 12:51:31 -0400

    fix build issue

--- docker-init.sh
+++ docker-init.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-java -Delastic.apm.server_urls=${ELASTIC_APM_SERVER_URLS} \
-     -Delastic.apm.verify_server_cert=${ELASTIC_APM_VERIFY_SERVER_CERT} \
-        -jar /opt/voter-service-0.2.0.jar \
+java -jar /opt/voter-service-0.2.0.jar \
         --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
         --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
