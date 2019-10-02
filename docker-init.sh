6e9ef85030e12284cafcff6942d75e80a88125da
Tom Qiang Wu <tom.q.wu@gmail.com>
2019-10-02 13:27:06 -0400

    Update docker-init.sh

--- docker-init.sh
+++ docker-init.sh
@@ -2,7 +2,7 @@
 
 if [ ${ENABLE_ELASTIC_JAVAAGENT} = "true" ];
 then
-        java -javaagent:/opt/elastic-apm-agent-1.9.0.jar
+        java -javaagent:/opt/elastic-apm-agent-1.9.0.jar \
                 -jar /opt/voter-service-0.2.0.jar \
                 --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
                 --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
