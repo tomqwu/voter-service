952f2da39f245c9af124e7d753c201177bb294c2
Tom Wu <tom.q.wu@gmail.com>
2021-03-10 17:41:45 -0500

    add helm

--- voter-service/docker-init.sh
+++ voter-service/docker-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 exec java -jar /opt/voter-service-0.2.0.jar \
-                --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
-                --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
+    --spring.profiles.active=${SPRING_PROFILE:-docker-production} \
+    --spring.data.mongodb.host=${MONGODB_HOST:-mongodb}
